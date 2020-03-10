2821fdc70132de95028f353fca7ced1f10a1b876
anisurrkhan <anisurk24@gmail.com>
2020-03-09 20:59:16 -0700

    car update form fields

--- frontend/components/cars/car_update.jsx
+++ frontend/components/cars/car_update.jsx
@@ -48,196 +48,153 @@ class CarUpdate extends React.Component {
   }
 
   render() {
-    return (
-      <div className="create-car-container">
-        <div className="create-car-banner">
-          <h3 className="create-banner-text">Update your car</h3>
-        </div>
 
-        <form className="create-car-form" onSubmit={this.handleSubmit}>
-          <h2>Your Car</h2>
-          <br />
-          <h3>Where is your car located?</h3>
-          <br />
+        return (
+            <div className="create-car-container">
+                <div className="create-car-banner">
+                    <h3 className="create-banner-text">List your car</h3>
+                </div>
 
-          <label>
-            Address
-            <input
-              type="text"
-              placeholder="Address"
-              value={this.props.address}
-              onChange={this.handleInput("address")}
-            ></input>
-          </label>
+            <form className="create-car-form" onSubmit={this.handleSubmit}>
 
-          <label>
-            City
-            <input
-              type="text"
-              placeholder="City"
-              value={this.props.city}
-              onChange={this.handleInput("city")}
-            ></input>
-          </label>
+                <h2>Your Car</h2>
+                    <br />
+                <h3>Where is your car located?</h3>
+                    <br />
 
-          <label>
-            State
-            <input
-              type="text"
-              placeholder="State"
-              value={this.props.state}
-              onChange={this.handleInput("state")}
-            ></input>
-          </label>
+                <label>Address
+                    <input type="text" placeholder="Address" value={this.props.address} onChange={this.handleInput("address")}></input>
+                </label>
 
-          <label>
-            Zip
-            <input
-              type="text"
-              placeholder="Zip"
-              value={this.props.zip}
-              onChange={this.handleInput("zip")}
-            ></input>
-          </label>
+                <label>City
+                    <input type="text" placeholder="City" value={this.props.city} onChange={this.handleInput("city")}></input>
+                </label>
 
-          <br />
+                <label>State
+                    <input type="text" placeholder="State" value={this.props.state} onChange={this.handleInput("state")}></input>
+                </label>
 
-          <h3>Which car do you have?</h3>
+                <label>Zip
+                    <input type="text" placeholder="Zip" value={this.props.zip} onChange={this.handleInput("zip")}></input>
+                </label>
 
-          <br />
+                <br />
 
-          <label>
-            Year
-            <input
-              type="text"
-              placeholder="Year"
-              value={this.props.year}
-              onChange={this.handleInput("year")}
-            />
-          </label>
+                <h3>Which car do you have?</h3>
 
-          <label>
-            Make
-            <input
-              type="text"
-              placeholder="Make"
-              value={this.props.make}
-              onChange={this.handleInput("make")}
-            />
-          </label>
+                <br />
 
-          <label>
-            Model
-            <input
-              type="text"
-              placeholder="Model"
-              value={this.props.model}
-              onChange={this.handleInput("model")}
-            />
-          </label>
+                <label>Year
+                    <input type="text" placeholder="Year" value={this.props.year} onChange={this.handleInput("year")} />
+                </label>
 
-          <label>
-            Color
-            <input
-              type="text"
-              placeholder="Color"
-              value={this.props.color}
-              onChange={this.handleInput("color")}
-            />
-          </label>
+                <label>Make
+                    <input type="text" placeholder="Make" value={this.props.make} onChange={this.handleInput("make")} />
+                </label>
 
-          <label>
-            Doors
-            <input
-              type="text"
-              placeholder="Number of doors"
-              value={this.props.doors}
-              onChange={this.handleInput("doors")}
-            />
-          </label>
+                <label>Model
+                    <input type="text" placeholder="Model" value={this.props.model} onChange={this.handleInput("model")} />
+                </label>
 
-          <label>
-            Seats
-            <input
-              type="text"
-              placeholder="Number of seats"
-              value={this.props.seats}
-              onChange={this.handleInput("seats")}
-            />
-          </label>
+                <label>Color
+                    <input type="text" placeholder="Color" value={this.props.color} onChange={this.handleInput("color")} />
+                </label>
 
-          <label>
-            License plate
-            <input
-              type="text"
-              placeholder="License plate"
-              value={this.props.plate}
-              onChange={this.handleInput("plate")}
-            />
-          </label>
+                <label>Doors
+                    <input type="text" placeholder="Number of doors" value={this.props.doors} onChange={this.handleInput("doors")} />
+                </label>
 
-          <br />
+                <label>Seats
+                    <input type="text" placeholder="Number of seats" value={this.props.seats} onChange={this.handleInput("seats")} />
+                </label>
 
-          <label>
-            Transmission
-            <br />
-            <input
-              type="radio"
-              value="automatic"
-              name="transmission"
-              onChange={this.handleInput("transmission")}
-            />
-            Automatic
-            <input
-              type="radio"
-              value="manual"
-              name="transmission"
-              onChange={this.handleInput("transmission")}
-            />
-            Manual
-            <input
-              type="radio"
-              value="none"
-              name="transmission"
-              onChange={this.handleInput("transmission")}
-            />
-            N/A
-          </label>
-          <br />
+                <label>License plate
+                    <input type="text" placeholder="License plate" value={this.props.plate} onChange={this.handleInput("plate")} />
+                </label>
 
-          <h2>Car Details</h2>
-          <br />
-          <br />
+                <br />
 
-          <label>
-            Daily Rate
-            <br />
-            <input
-              type="text"
-              placeholder="Daily Rate in (USD)"
-              value={this.props.rate}
-              onChange={this.handleInput("rate")}
-            />
-          </label>
+                <label>Transmission
+                <br />
+                    <input type="radio" value="automatic" name="transmission" onChange={this.handleInput("transmission")} />Automatic
+                    <input type="radio" value="manual" name="transmission" onChange={this.handleInput("transmission")} />Manual
+                    <input type="radio" value="none" name="transmission" onChange={this.handleInput("transmission")} />N/A
+                </label>
+                <br />
 
-          <br />
+                <h2>Car Details</h2>
 
-          <label>
-            Description
-            <br />
-            <textarea
-              placeholder="A detailed description will get you more trips!"
-              value={this.props.description}
-              onChange={this.handleInput("description")}
-            ></textarea>
-          </label>
+                <br />
+                <br />
 
-          <br />
+                <label>Daily Rate
+                <br />
+                    <input type="text" placeholder="Daily Rate in (USD)" value={this.props.rate} onChange={this.handleInput("rate")} />
+                </label>
 
-          <input className="car-finish-btn" type="submit" value="Finish" />
+                <br />
 
-          <span className="car-errors">{this.renderErrors()}</span>
-        </form>
+                <label>Description
+                <br />
+                    <textarea placeholder="A detailed description will get you more trips!" value={this.props.description} onChange={this.handleInput("description")}></textarea>
+                </label>
+
+                <h3>Features</h3>
+                    <div className="features-wrapper">
+                    <div>
+                    <label><input type="checkbox" value={this.state.awd} onChange={this.handleCheckbox("awd")} /> All-wheel-drive</label>
+                        <br/>
+                        <label><input type="checkbox" value={this.state.audioInput} onChange={this.handleCheckbox("audioInput")} /> Audio input</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.bikeRack} onChange={this.handleCheckbox("bikeRack")} /> Bike rack</label>
+                        <br />
+                    </div>
+                    <div>              
+                        <label><input type="checkbox" value={this.state.bluetooth} onChange={this.handleCheckbox("bluetooth")} /> Bluetooth</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.childSeat} onChange={this.handleCheckbox("childSeat")} /> Child seat</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.convertible} onChange={this.handleCheckbox("convertible")} /> Convertible</label>
+                        <br />
+                    </div>
+                    <div>            
+                        <label><input type="checkbox" value={this.state.gps} onChange={this.handleCheckbox("gps")} /> GPS</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.heatedSeats} onChange={this.handleCheckbox("heatedSeats")} /> Heated seats</label>
+                        <br/>
+                        <label><input type="checkbox" value={this.state.longtermCar} onChange={this.handleCheckbox("longtermCar")} /> Longterm car</label>
+                        <br />
+                    </div>
+                    <div>            
+                        <label><input type="checkbox" value={this.state.petFriendly} onChange={this.handleCheckbox("petFriendly")} /> Pet friendly</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.skiRack} onChange={this.handleCheckbox("skiRack")} /> Ski rack</label>
+                        <br/>
+                        <label><input type="checkbox" value={this.state.snowTiresChains} onChange={this.handleCheckbox("snowTiresChains")} /> Snow tires/Chains</label>
+                        <br />
+                    </div>
+                    <div>            
+                        <label><input type="checkbox" value={this.state.sunroof} onChange={this.handleCheckbox("sunroof")} /> Sunroof</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.tollPass} onChange={this.handleCheckbox("tollPass")} /> Toll pass</label>
+                        <br />
+                        <label><input type="checkbox" value={this.state.usbInput} onChange={this.handleCheckbox("usbInput")} /> USB input</label>
+                        <br />
+                    </div>
+                    </div>
+                <br/>
+
+                <br />
+
+                <input type="file" onChange={e => this.setState({ photos: e.target.files })} multiple />
+
+                <br />
+
+                <input className="car-finish-btn" type="submit" value="Finish" />
+
+                <span className="car-errors">{this.renderErrors()}</span>
+
+            </form>
       </div>
     );
   }
